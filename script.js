7ce5457a768e556ed1e9c0ad8d2a2f3217ac7f1a
Agostina Ramos <Agosramos_12@hotmail.com>
2023-07-05 09:54:44 -0300

    Modifique cards

--- script.js
+++ script.js
@@ -13,16 +13,17 @@ fetch("https://www.omdbapi.com/?apikey=d667fc14&t=Iron%20Man")
         document.getElementById("imgpeli2").src = data.Poster;
     });    
 
+    // fetch the advengers
+fetch("https://www.omdbapi.com/?apikey=d667fc14&t=The%20Avengers")
+.then((response) => response.json())
+.then((data) => {
+    document.getElementById("imgpeli3").src = data.Poster;
+});
+
 // fetch la la land
 fetch("https://www.omdbapi.com/?apikey=d667fc14&t=La%20La%20Land")
     .then((response) => response.json())
     .then((data) => {
-        document.getElementById("imgpeli3").src = data.Poster;
-    });
-
-// fetch the advengers
-fetch("https://www.omdbapi.com/?apikey=d667fc14&t=The%20Avengers")
-    .then((response) => response.json())
-    .then((data) => {
         document.getElementById("imgpeli4").src = data.Poster;
     });
+
